807bb171a588903ffa8e7a2eeccc1fdc1cc1d9e7
Sanyog Singh Rajput <sanyogsingrajput@gmail.com>
2024-04-09 22:14:33 +0530

    git testing

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -12,7 +12,7 @@ const Discover = () => {
      <div className="flex flex-col">
         <div className="w-full fex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
 
-            <h2 className="font-bold text-3xl text-white text-left">
+            {/* <h2 className="font-bold text-3xl text-white text-left"> */}
                 Discover
             </h2>
             <select />
