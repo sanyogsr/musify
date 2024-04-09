597c4becf79140d44c1a08221e651010b61cfd8f
Sanyog Singh Rajput <166251568+sanyogssrr@users.noreply.github.com>
2024-04-09 22:06:31 +0530

    project added

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,23 @@
+
+import {Error, Loader,SongCard} from "../components";
+import {genres} from "../assets/constants"
+
+
+
+const Discover = () => {
+     console.log("object");
+
+
+ return( <div>
+     <div className="flex flex-col">
+        <div className="w-full fex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+
+            <h2 className="font-bold text-3xl text-white text-left">
+                Discover
+            </h2>
+            <select />
+        </div>
+     </div>
+</div>);}
+ 
+export default Discover;
